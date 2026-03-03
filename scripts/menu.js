532a5843df482631b04c41efe85173b8484d05d9
KeystoneEconomics <Rebecca.Snow@keystoneeconomics.co.uk>
2026-03-03 16:52:16 +0000

    New files

---
+++ scripts/menu.js
@@ -0,0 +1,11 @@
+document.write('<nav class="navbar navbar-expand-md bg-nav sticky-top"><div class="container-fluid"><img src="resources/keystonelogo.png" onclick="window.location=\'index.html\'" /><a class="navbar-brand text-black ms-2 mobileHidden" href="index.html">Keystone Economics</a><button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse" id="navbarSupportedContent">');
+document.write('<ul class="navbar-nav me-auto mb-2 mb-lg-0">');
+document.write('<li class="nav-item"><a class="nav-link text-black" href="index.html">Home</a></li>');
+
+document.write('<li class="nav-item"><a class="nav-link text-black " href="about.html">About</a></li>');
+
+document.write('<li class="nav-item"><a class="nav-link text-black" href="team.html">Contact Us</a></li>');
+
+document.write('</ul>');
+
+
